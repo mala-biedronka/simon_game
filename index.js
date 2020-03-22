25f6609a0213de864a915938eab1f783ce0f9b92
Iryna Predborska <iryna.predborska@gmail.com>
2020-03-22 18:14:54 +0100

    add rendomNumber function

---
+++ index.js
@@ -0,0 +1,16 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+//Generates a random number between 0 and 3.
+let randomNumber = function nextSequence () {
+    return Math.floor(Math.random() * 4);
+};
+
+//4. Create a new variable called randomChosenColour
+// and use the randomNumber from step 2 to select
+// a random colour from the buttonColours array.
+
+let randomChosenColour 
+
+//5. At the top of the game.js file, create a new empty array called gamePattern.
+
+// 6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
