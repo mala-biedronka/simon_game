6e962f001738d9f6221f45edf80764ff35bdf190
Iryna Predborska <iryna.predborska@gmail.com>
2020-03-29 19:13:52 +0200

    add animation

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
+let userClickedPattern = [];
+let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 
+const fadeTime = 150;
+
 //Generates a random number between 0 and 3.
-let randomNumber = function nextSequence () {
-    return Math.floor(Math.random() * 4);
-};
+function nextSequence () {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(fadeTime).fadeIn(fadeTime);
+    playSound(randomChosenColour);
+}
 
-//4. Create a new variable called randomChosenColour
-// and use the randomNumber from step 2 to select
-// a random colour from the buttonColours array.
+$(".btn").click(function (event) {
+    let userChosenColour = event.target.id;
+    userClickedPattern.push(userChosenColour);
+    animatePress(userChosenColour);
+});
 
-let randomChosenColour 
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
-//5. At the top of the game.js file, create a new empty array called gamePattern.
 
-// 6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+
+//Add Animations to User Clicks
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+
+//Detects when a keyboard key has been pressed
+$(document).keypress(function () {
+    //You'll need a way to keep track of whether if the game has started
+    // or not, so you only call nextSequence() on the first keypress.
+    let level = 0;
+    nextSequence ();
+})
+
+//Debug
+
+// if(window.attachEvent) {
+//     window.attachEvent('onload', yourFunctionName);
+// } else {
+//     if(window.onload) {
+//         var curronload = window.onload;
+//         var newonload = function(evt) {
+//             curronload(evt);
+//             nextSequence(evt);
+//         };
+//         window.onload = newonload;
+//     } else {
+//         window.onload = nextSequence;
+//     }
+// }
